d1c3aaa5aa81cea55aa8fe61c2d9bbbd75da4e9b
lasha <maisuradze.llasha@gmail.com>
2025-08-24 18:55:01 +0400

    lala laptop run out of battery

--- calculator.js
+++ calculator.js
@@ -1,8 +1,14 @@
 
 let keys = document.querySelectorAll(".Divs");
+let screeen = document.querySelector(".screen");
+let onScreen;
 
 keys.forEach(key => {
-    key.addEventListener("click", function() {
+    let inp = key.addEventListener("click", function() {
         console.log("works:", key.innerText);
     });
 });
+
+screeen.innerHTML = `
+<p>${key.innerText}</p>   
+`
